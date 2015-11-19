f6e560de6006d077fa2302186f29136ec8512dab
RikiRusli <7314032@student.unpar.ac.id>
2015-11-19 15:43:39 +0700

    Create BlackHole.java

---
+++ java/BlackHole.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i14032
+ */
+public class BlackHole extends Locator {
+
+    public BlackHole(int x, int y, int xOut, int yOut) {
+        super(x, y, xOut, yOut);
+    }
+
+}
