9ee71ea324058ff525f81737138237ad05f21e99
Daniel Ferdinan <7314038@student.unpar.ac.id>
2015-11-20 12:47:03 +0700

    Update Kelas Astronot

--- php/Astronot.php
+++ php/Astronot.php
@@ -17,7 +17,7 @@
 		# Konstruktor dari kelas Astronot
 		# @param name masukan nama astronot (pemain) - String
 		public function __construct($nameInput) {
-                $this->name=$nameInput;
+                	$this->name=$nameInput;
         	}
 		
 		# Getter dari nama astronot
@@ -44,22 +44,31 @@
 			return $this->locate+1;
 		}
 		
+		# Getter dari status astronot (pemain) - Menang/Kalah
+		# @return status menang/kalah - boolean
 		public function is_Win_As() {
         		return $this->isWin;
 		}
 
+		# Setter dari nama astronot (pemain)
+		# @param name masukan nama astronot (pemain) - String
 		public function set_Name($name){
 			$this->name = $name;
 		}
 
+		# Setter untuk posisi X (sumbu kartersian) astronot
+		# @param x masukan posisi X - int
 		public function set_X_As($xInput){
 			$this->x = $xInput;
 		}
 
+		# Setter untuk posisi Y (sumbu kartersian) astronot
+		# @param y masukan posisi Y - int
 		public function set_Y_As($yInput){
 			$this->y = $yInput;
 		}
 
+		# Setter dari posisi astronot (pemain) pada board Interstellar - void
 		public function set_Locate_As(){
 			if ($this->y % 2 == 0) {
 				//System.out.println("ini");
@@ -69,13 +78,15 @@
 				//System.out.println("itu"+$this->x+" "+$this->y);
 				$this->locate = (9 - $this->y) * 10 + $this->x;
 			}
-
 		}
 		
+		# Setter dari status astronot (pemain) - Menang/Kalah
+		# @param isWin masukan status dari astronot - boolean
 		public function set_Win_As($isWin){
 			$this->isWin = $isWin;
 		}
 		
+		# Method untuk menggerakkan-pindah posisi astronot (pemain) pada Interstellar
 		public function move() {
 			//saat x=9 maka y mines 1
 			if ($this->get_Y_As() % 2 != 0) {
@@ -96,12 +107,18 @@
 					//this.setLocateAs(locate+1);
 				}
 			}
-        }
+        	}
 		
+		# Method untuk membuat astronot (pemain) bergerak mundur, jika sudah mencapai finish
+		# namun masih belum bergerak sesuai dengan angka yang ada pada dadu.
+		# @param dadu masukan angka pada dadu - int
 		public function move_Backward($dadu){
 			$this->set_X_As($dadu-$this->x);
 		}
 		
+		# Method untuk menentukan astronot (pemain) sudah menang atau belum
+		# Menang berarti sudah mencapai finish (100 pada board Interstellar) lebih dulu dibanding pemain lainnya
+		# @return status astronot menang/kalah - boolean
 		public function is_Win() {
 			if ($this->get_Locate_As() == 100) {
 				return true;
